a166cad7cc9a26d88567b18e59193ff5352a0332
runabh <runabh.d@gmail.com>
2020-10-30 16:24:55 +0530

    snipcart trial

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+app.use(express.static(path.join(__dirname, "build")));
+app.get("/v2", function (req, res) {
+  res.sendFile(path.join(__dirname, "build", "v2.html"));
+});
+app.get("/v3", function (req, res) {
+  res.sendFile(path.join(__dirname, "build", "v3.html"));
+});
+app.get("/iframe", function (req, res) {
+  res.sendFile(path.join(__dirname, "build", "iframe.html"));
+});
+const PORT = process.env.PORT || 3000;
+app.listen(PORT);
